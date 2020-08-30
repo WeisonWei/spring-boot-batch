8479d2f64b750417c0e27d5b5a7a889a6725b460
WeisonWei <weixx3@126.com>
2020-08-30 12:21:37 +0800

    init

---
+++ src/test/java/com/weison/sbb/SpringBootBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weison.sbb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
